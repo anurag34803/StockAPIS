e114dbcb982e6f3d036c449150746a4276001db2
anurag34803 <48412537+anurag34803@users.noreply.github.com>
2024-04-05 13:52:01 +0530

    Update Main.java

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -17,10 +17,21 @@ public class Main {
     static List<String> c1 = List.of("","openPrice", "lowPrice", "highPrice", "currentPrice", "baseVolume", "quoteVolume", "time");
 
     // Column name for market depth
-    static List<String> c2 = List.of("","time", "bids", "asks");
-    static String coins[] = {"QKC/INR","RLC/INR","MTL/INR"};
+    static List<String> c2 = List.of("","time", "bids", "asks", "bids%", "asks%");
+    static String coins[] = {"QKC/INR","RLC/INR","MTL/INR",
+            "AVAX/INR","SOL/INR","ANKR/INR",
+            "ETC/INR","APT/INR","THETA/INR"};
 
     public static void main(String[] args) throws Exception {
+
+        Redirect System.out to null output stream
+        System.setOut(new PrintStream(new OutputStream() {
+           @Override
+           public void write(int b) throws IOException {
+               // Do nothing, suppress output
+           }
+       }));
+
         ScheduledExecutorService scheduler = Executors.newScheduledThreadPool(1);
 
         // Schedule the task to run every 1 minute
@@ -30,10 +41,10 @@ public class Main {
                 try {
                     ArrayList<String> coinData = get24hrSinglePairData(coin);
                     writeDataToExcel(coinData,c1,"CoinData.xlsx");
-                    System.out.println("Data written to Excel successfully. for coin " + coin);
+                    //System.out.println("Data written to Excel successfully. for coin " + coin);
                     ArrayList<String> Depth = getDepthOfCoin(coin);
                     writeDataToExcel(Depth,c2,"MarketDepth.xlsx");
-                    System.out.println("Data written to Excel successfully. for coin " + coin);
+                    //System.out.println("Data written to Excel successfully. for coin " + coin);
 
                 } catch (Exception e) {
                     System.err.println("Error writing data to Excel: " + e.getMessage());
